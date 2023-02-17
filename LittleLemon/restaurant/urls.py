5eae01b2fcc215a4b1a9b6c383ae7d69c3c02269
dsit33 <dereksit@yahoo.com>
2023-02-16 17:48:28 -0800

    add serializers and api views for menu items and bookings

--- LittleLemon/restaurant/urls.py
+++ LittleLemon/restaurant/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('menu/', views.MenuItemView.as_view()),
+    path('menu/<int:pk>', views.SingleItemView.as_view()),
+    path('booking/', views.BookingView.as_view()),
 ]
--- LittleLemon/restaurant/views.py
+++ LittleLemon/restaurant/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics, viewsets
+from rest_framework.response import Response
+from .serializers import BookingSerializer, MenuItemSerializer
+from .models import Booking, MenuItem
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html', {})
+
+class MenuItemView(generics.ListCreateAPIView):
+    queryset = MenuItem.objects.all()
+    serializer_class = MenuItemSerializer
+
+class SingleItemView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = MenuItem.objects.all()
+    serializer_class = MenuItemSerializer
+
+class BookingView(generics.ListCreateAPIView, generics.DestroyAPIView):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
